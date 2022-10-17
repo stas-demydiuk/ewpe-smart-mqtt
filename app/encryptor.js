576cab9d1f8bfe06dc5a8fff0fcc6ba704770fd5
Xenomes <xenomes@outlook.com>
2022-10-17 20:29:22 +0200

    Fixed communication with higher firmware. Change encoding/decoding and some clean up.

--- app/encryptor.js
+++ app/encryptor.js
@@ -1,20 +1,29 @@
-const crypto = require('crypto');
+const MCrypt = require('mcrypt').MCrypt;
 
 const defaultKey = 'a3K8Bx%2r8Y7#xDh';
 
 function encrypt(data, key = defaultKey) {
-    const cipher = crypto.createCipheriv('aes-128-ecb', key, '');
-    const str = cipher.update(JSON.stringify(data), 'utf8', 'base64');
-    const request = str + cipher.final('base64');
-    return request;
+	const desEcb = new MCrypt('rijndael-128', 'ecb');
+	desEcb.open(key);
+	console.log(JSON.stringify(data));
+	const ciphertext = desEcb.encrypt(JSON.stringify(data));
+	const request = ciphertext.toString('base64');
+	return request;
 }
 
 function decrypt(data, key = defaultKey) {
-    const decipher = crypto.createDecipheriv('aes-128-ecb', key, '');
-    const str = decipher.update(data, 'base64', 'utf8');
-    const response = JSON.parse(str + decipher.final('utf8'));
-
-    return response;
+	const aesEcb = MCrypt('rijndael-128', 'ecb')
+	aesEcb.open(key);
+	const ciphertext = new Buffer(data, 'base64');
+	const plaintext = aesEcb.decrypt(ciphertext).toString();
+	if (plaintext.includes("�")) {null} else {
+	//console.log(plaintext);
+        const response = plaintext.slice(0,plaintext.lastIndexOf('}'))+'}';
+	result = JSON.parse(response.toString());
+	result.cid = result.cid || result.mac;
+	result.name = result.name || result.mac.substring(result.mac.length - 8);
+	}
+	return result
 }
 
 module.exports = {
